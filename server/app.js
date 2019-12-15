4938eeac760c660e5ac1d3175d6b2cbe0561b0d5
arenHome <aren.aren.97@mail.ru>
2019-12-15 18:44:49 +0300

    Настройка сервера

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+//
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+//
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
+
+module.exports = {
+  app,
+  server
+}
+
