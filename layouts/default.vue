853d834bf7d7dc918e12c60a55695625669c64d4
arenHome <aren.aren.97@mail.ru>
2019-12-22 14:37:44 +0300

    Верстка страницы чата. зашита роута чата. Верстка первой страницы с формой. Логика соединения пользователя к комнате и отправка сообщений от админа. хранения данных пользователя и сообщений.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,69 @@
 <template>
   <v-app app>
-    <nuxt />
+    <v-navigation-drawer app v-model="drawer">
+      <v-list subheader>
+        <v-subheader>Список людей в комнате</v-subheader>
+
+        <v-list-item
+          v-for="u in users"
+          :key="u.id"
+          @click=""
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{u.name}}</v-list-item-title>
+          </v-list-item-content>
+
+          <v-list-item-icon>
+            <v-icon :color="u.id === 2 ? 'primary' : 'grey'">chat_bubble</v-icon>
+          </v-list-item-icon>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="toggleMenu"></v-app-bar-nav-icon>
+      <v-btn icon @click="exit">
+        <v-icon>arrow_back</v-icon>
+      </v-btn>
+
+      <v-toolbar-title>Чат комнаты {{user.room}}</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <div>
+        <nuxt />
+      </div>
+    </v-content>
+
   </v-app>
 </template>
 
 <script>
-export default { }
+  import { mapState, mapMutations } from 'vuex'
+  export default {
+    data () {
+      return {
+        drawer: false,
+        users: [
+          { id: 1, name: 'User 1' },
+          { id: 2, name: 'User 2' },
+          { id: 3, name: 'User 3' },
+          { id: 4, name: 'User 4' },
+          { id: 5, name: 'User 5' }
+        ]
+      }
+    },
+    computed: {
+      ...mapState(['user'])
+    },
+    methods: {
+      ...mapMutations(['clearData']),
+      exit () {
+        this.$router.push('/?message=leftChat')
+        this.clearData()
+      },
+      toggleMenu () {
+        this.drawer = !this.drawer
+      }
+    }
+  }
 </script>
--- server/app.js
+++ server/app.js
@@ -1,9 +1,23 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+const m = (name, text, id) => ({name, text, id})
 
 io.on('connection', socket => {
-  console.log('IO connected')
+
+  socket.on('userJoined', (data, callback) => {
+    if (!data.name || !data.room) {
+      return callback({ valid: false, text: 'Данные не верные' })
+    }
+
+    socket.join(data.room)
+    callback({ valid: true, userId: socket.id })
+    socket.emit('newMessage', m('admin', `Добро пожаловать ${data.name}`))
+    socket.broadcast
+      .to(data.room)
+      .emit('newMessage', m('admin', `Пользователь ${data.name} зашел.`))
+  })
 
   socket.on('createMessage', data => {
     setTimeout(() => {
@@ -12,6 +26,8 @@ io.on('connection', socket => {
       })
     }, 500)
   })
+
+
 })
 
 module.exports = {
