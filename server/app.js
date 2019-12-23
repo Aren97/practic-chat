7cf36abb42ab24fa610ce714a946685b98628224
arenHome <aren.aren.97@mail.ru>
2019-12-23 06:54:56 +0300

    Отправка сообщений всем пользователям комнаты, формирование списка пользователей комнаты. Добавление и удаление пользователей.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const users = require('./users')()
 
 const m = (name, text, id) => ({name, text, id})
 
@@ -12,6 +13,14 @@ io.on('connection', socket => {
     }
 
     socket.join(data.room)
+
+    users.remove(socket.id)
+    users.add({
+      id: socket.id,
+      name: data.name,
+      room: data.room
+    })
+
     callback({ valid: true, userId: socket.id })
     socket.emit('newMessage', m('admin', `Добро пожаловать ${data.name}`))
     socket.broadcast
@@ -19,15 +28,19 @@ io.on('connection', socket => {
       .emit('newMessage', m('admin', `Пользователь ${data.name} зашел.`))
   })
 
-  socket.on('createMessage', data => {
-    setTimeout(() => {
-      socket.emit('newMessage', {
-        text: data.text + ' SERVER'
-      })
-    }, 500)
+  socket.on('createMessage', (data, callback) => {
+    if (!data.text) {
+      return callback({ valid: false, text: 'текст не может быть пустым' })
+    }
+
+    const user = users.get(data.id)
+
+    if (user) {
+      io.to(user.room).emit('newMessage', m(user.name, data.text, user.id))
+    }
+
+    return callback({ valid: true, text: 'Сообщение отправлено' })
   })
-
-
 })
 
 module.exports = {
